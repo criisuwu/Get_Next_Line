ebefcc17c038b023f2b00f5cc86da1e83a186fb0
Cristina Homobono Fernandez <chomobon@c1r4s6.42madrid.com>
2024-07-19 13:09:47 +0200

    Solo he modificado la funcion de lectura pero sigue leyendo null

--- get_next_line.c
+++ get_next_line.c
@@ -6,37 +6,44 @@
 /*   By: chomobon <chomobon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 11:04:22 by chomobon          #+#    #+#             */
-/*   Updated: 2024/07/16 18:12:04 by chomobon         ###   ########.fr       */
+/*   Updated: 2024/07/19 13:08:50 by chomobon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+char *ft_line(char *str)
+{
+    
+}
+// Esta funcion es solo de lectura, y para ir almacenando las lecturas
 char *ft_read(int fd, char *reserv)
 {
     char    *buff;
     int     verify;
+    char    *aux;
+    char    *keeper;
 
     verify = 1;
-    buff = malloc(sizeof(char) * BUFFER_SIZE + 1);
-    if(!buff)
-        return (NULL);
+    aux = ft_substr(reserv, 0, ft_strlen(reserv));
     while(!ft_strchr(buff, '\n') && verify > 0)
     {
+        buff = malloc(sizeof(char) * BUFFER_SIZE + 1);
+        if(!buff)
+            return (NULL);
         verify = read(fd, buff, BUFFER_SIZE);
         if (verify <= 0)
-        {
-            free (buff);
-            free(reserv);
-            return (NULL);
-        }
+            return (free (buff), free(reserv), NULL);
         buff[verify] = '\0';
-        reserv = ft_strjoin(reserv, buff);
-        printf("buff: %s\n", buff);
-        printf("reserv: %s\n", reserv);
+        keeper = aux;
+        aux = ft_strjoin(aux, buff);
+        free(keeper);
+        free(aux);
+        // printf("buff: %s\n", buff);
+        // printf("reserv: %s\n", reserv);
+        // printf("aux: %s\n", aux);
     }
-    free (buff);
-    return (reserv);
+    return (free (buff), reserv);
 }
 
 char    *get_next_line(int fd)
@@ -109,14 +116,11 @@ int main ()
 {
     char *line;
     int fd = open("a.txt", O_RDONLY);
-    //char *str = ft_read(op);
-    //printf("Lo del archivo: %s\n", str);
     while ((line = get_next_line(fd)) != NULL)
     {
         // printf("entra bucle");
         printf("%s", line);
         free(line);
-        // break;
     }
     close(fd);
     return (0);
